e16de871116d43167bada1a79557c4ff00b5e71b
Daniel Gómez i Sañé <daniel.gomez.sane@gmail.com>
2021-01-06 20:11:26 +0100

    Day14part1 binary ops

--- Day14/Day14.cpp
+++ Day14/Day14.cpp
@@ -7,13 +7,15 @@
 #include <fstream>
 #include <unordered_map>
 
-std::string DecimalToBinary(uint64_t decimal);
-uint64_t BinaryToDecimal(std::string binary);
+//std::string DecimalToBinary(uint64_t decimal);
+//uint64_t BinaryToDecimal(std::string binary);
 
 int main()
 {
     std::string line;
-    std::string mask;
+    std::string smask;
+    uint64_t applymask;
+    uint64_t ignoremask;
     std::ifstream input;
     std::unordered_map<uint32_t, uint64_t> map;
     input.open("input.txt");
@@ -22,25 +24,37 @@ int main()
     {
         if (line.find("mask") != line.npos)
         {
-            mask = line.substr(7);
+            smask = line.substr(7);
+            std::string s_applymask = smask;
+            std::string s_ignoremask = smask;
+            for (int i = 0; i < smask.length(); ++i)
+            {
+                if (smask[i] == 'X')
+                {
+                    s_ignoremask[i] = '1';
+                    s_applymask[i] = '0';
+                }
+                else
+                {
+                    s_ignoremask[i] = '0';
+                    if (smask[i] == '1')
+                        s_applymask[i] = '1';
+                    else
+                        s_applymask[i] = '0';
+                }
+            }
+            applymask = std::stoull(s_applymask, 0, 2); //convert to binary unsigned int
+            ignoremask = std::stoull(s_ignoremask, 0, 2); //convert to binary unsigned int
             continue;
         }
 
         auto pos1 = line.find("[") + 1;
         auto pos2 = line.find("]");
-        uint32_t mem = stoi(line.substr(pos1, (pos2 - pos1)));
-        uint64_t decimal = stoi(line.substr(pos2 + 4));
-        std::string bin = DecimalToBinary(decimal);
-        for (int i = 0; i < mask.length(); ++i)
-        {
-            if (mask[i] == 'X')
-                continue;
+        uint32_t mem = stoul(line.substr(pos1, (pos2 - pos1)));
+        uint64_t val = stoull(line.substr(pos2 + 4));
 
-            bin[i] = mask[i];
-        }
-
-        decimal = BinaryToDecimal(bin);
-        map[mem] = decimal;
+        val = (val & ignoremask) | applymask;
+        map[mem] = val;
     }
 
     uint64_t part1 = 0;
@@ -50,33 +64,33 @@ int main()
     std::cout << "Part1: " << part1 << "\n";
 }
 
-std::string DecimalToBinary(uint64_t decimal)
-{
-    int result[36] = {0}; //The bitmask is always given as a string of 36 bits
-    std::string resultr;
-    for (int i = 0; decimal > 0; i++)
-    {
-        result[i] = decimal % 2;
-        decimal /= 2;
-    }
-
-    for (int i = 35; i >= 0; --i)
-        resultr.append(std::to_string(result[i]));
-        
-    return resultr;
-}
-
-uint64_t BinaryToDecimal(std::string binary)
-{
-    binary = binary.substr(binary.find("1"));
-    uint64_t result = 0;
-    for (uint64_t i=0 ; i<binary.length(); ++i)
-    {
-        if (binary[i] == '1')
-        {
-            uint64_t j = binary.length() - i - 1;
-            result += pow(2, j);
-        }
-    }
-    return result;
-}
+//std::string DecimalToBinary(uint64_t decimal)
+//{
+//    int result[36] = {0}; //The bitmask is always given as a string of 36 bits
+//    std::string resultr;
+//    for (int i = 0; decimal > 0; i++)
+//    {
+//        result[i] = decimal % 2;
+//        decimal /= 2;
+//    }
+//
+//    for (int i = 35; i >= 0; --i)
+//        resultr.append(std::to_string(result[i]));
+//        
+//    return resultr;
+//}
+//
+//uint64_t BinaryToDecimal(std::string binary)
+//{
+//    binary = binary.substr(binary.find("1"));
+//    uint64_t result = 0;
+//    for (uint64_t i=0 ; i<binary.length(); ++i)
+//    {
+//        if (binary[i] == '1')
+//        {
+//            uint64_t j = binary.length() - i - 1;
+//            result += pow(2, j);
+//        }
+//    }
+//    return result;
+//}
