cccd393bb8b018034d944e91c1e45e464237db8f
Daniel Gómez i Sañé <daniel.gomez.sane@gmail.com>
2021-01-12 00:26:24 +0100

    cleanup

--- Day18/Day18.cpp
+++ Day18/Day18.cpp
@@ -6,49 +6,38 @@
 #include <string>
 #include <sstream>
 
-uint64_t Part1();
-uint64_t Part2();
+uint64_t Part(int i);
 uint64_t Solve1(std::string& str);
 uint64_t Solve2(std::string& str);
 void operate(std::string& str);
+void SolveSums(std::string& deepop);
 void operatesum(std::string& str);
-int CalculateDeepMapParentheses(const std::string& str, std::string& strmap);
-int CalculateDeepMapSum(const std::string& str, std::string& strmap);
+int CalculateDepthMapParentheses(const std::string& str, std::string& strmap);
+int CalculateDepthMapSum(const std::string& str, std::string& strmap);
 
 int main()
 {
-    std::cout << "Part 1 solution: " << Part1() << "\n";
-    std::cout << "Part 2 solution: " << Part2() << "\n";
+    std::cout << "Part 1 solution: " << Part(1) << "\n";
+    std::cout << "Part 2 solution: " << Part(2) << "\n";
+}
+uint64_t Part(int i)
+{
+    uint64_t result = 0;
+    std::ifstream input;
+    input.open("input.txt");
+    std::string line;
+    auto function = i == 1 ? Solve1 : Solve2;
+    while (getline(input, line))
+    {
+        result += function(line);
+    }
+    return result;
 }
 
-uint64_t Part1()
-{
-    uint64_t result = 0;
-    std::ifstream input;
-    input.open("input.txt");
-    std::string line;
-    while (getline(input, line))
-    {
-        result += Solve1(line);
-    }
-    return result;
-}
-uint64_t Part2()
-{
-    uint64_t result = 0;
-    std::ifstream input;
-    input.open("input.txt");
-    std::string line;
-    while (getline(input, line))
-    {
-        result += Solve2(line);
-    }
-    return result;
-}
 uint64_t Solve1(std::string& str)
 {
     std::string strmap;
-    int maxdeep = CalculateDeepMapParentheses(str, strmap);
+    int maxdeep = CalculateDepthMapParentheses(str, strmap);
 
     while (maxdeep >= 0)
     {
@@ -61,7 +50,7 @@ uint64_t Solve1(std::string& str)
             std::string deepop = str.substr(pos1, pos2 - pos1);
             operate(deepop);
             str.replace(pos1, pos2 - pos1, deepop);
-            maxdeep = CalculateDeepMapParentheses(str, strmap);
+            maxdeep = CalculateDepthMapParentheses(str, strmap);
         }
         else
         {
@@ -74,7 +63,7 @@ uint64_t Solve1(std::string& str)
 uint64_t Solve2(std::string& str)
 {
     std::string strmap;
-    int maxdeep = CalculateDeepMapParentheses(str, strmap);
+    int maxdeep = CalculateDepthMapParentheses(str, strmap);
 
     while (maxdeep >= 0)
     {
@@ -87,41 +76,15 @@ uint64_t Solve2(std::string& str)
             std::string deepop = str.substr(pos1, pos2 - pos1);
             deepop.erase(deepop.begin());
             deepop.erase(deepop.end() - 1);
-            std::string strmap2;
-            int maxdeepsum = CalculateDeepMapSum(deepop, strmap2);
-            while (maxdeepsum > 0)
-            {
-                std::string level = std::to_string(maxdeepsum);
-                int pos1s = strmap2.find(level);
-                int pos2s = strmap2.find_first_not_of(level, pos1s);
-                if (pos2s == strmap2.npos)
-                    pos2s = strmap2.length();
-                std::string deepopsum = deepop.substr(pos1s, pos2s - pos1s);
-                operatesum(deepopsum);
-                deepop.replace(pos1s, pos2s - pos1s, deepopsum);
-                maxdeepsum = CalculateDeepMapSum(deepop, strmap2);
-            }
 
+            SolveSums(deepop);
             operate(deepop);
             str.replace(pos1, pos2 - pos1, deepop);
-            maxdeep = CalculateDeepMapParentheses(str, strmap);
+            maxdeep = CalculateDepthMapParentheses(str, strmap);
         }
         else
         {
-            std::string strmap2;
-            int maxdeepsum = CalculateDeepMapSum(str, strmap2);
-            while (maxdeepsum > 0)
-            {
-                std::string level = std::to_string(maxdeepsum);
-                int pos1s = strmap2.find(level);
-                int pos2s = strmap2.find_first_not_of(level, pos1s);
-                if (pos2s == strmap2.npos)
-                    pos2s = strmap2.length();
-                std::string deepopsum = str.substr(pos1s, pos2s - pos1s);
-                operatesum(deepopsum);
-                str.replace(pos1s, pos2s - pos1s, deepopsum);
-                maxdeepsum = CalculateDeepMapSum(str, strmap2);
-            }
+            SolveSums(str);
             operate(str);
             return stoull(str);
         }
@@ -130,7 +93,7 @@ uint64_t Solve2(std::string& str)
 
 void operate(std::string& str)
 {
-    //Erase parentesis
+    ////Erase parentesis
     if (str.find("(") != str.npos)
     {
         str.erase(str.begin());
@@ -147,7 +110,6 @@ void operate(std::string& str)
     while (!ss.eof())
     {
         ss >> stmp;
-
         if (first)
         {
             num1 = stoi(stmp);
@@ -164,13 +126,30 @@ void operate(std::string& str)
         else
             std::stringstream(stmp) >> op;
     }
-
     str = std::to_string(num1);
+}
+
+void SolveSums(std::string& deepop)
+{
+    std::string strmap2;
+    int maxdeepsum = CalculateDepthMapSum(deepop, strmap2);
+    while (maxdeepsum > 0)
+    {
+        std::string level = std::to_string(maxdeepsum);
+        int pos1s = strmap2.find(level);
+        int pos2s = strmap2.find_first_not_of(level, pos1s);
+        if (pos2s == strmap2.npos)
+            pos2s = strmap2.length();
+        std::string deepopsum = deepop.substr(pos1s, pos2s - pos1s);
+        operatesum(deepopsum);
+        deepop.replace(pos1s, pos2s - pos1s, deepopsum);
+        maxdeepsum = CalculateDepthMapSum(deepop, strmap2);
+    }
 }
 
 void operatesum(std::string& str)
 {
-    //Erase parentesis
+    ////Erase parentesis
     if (str.find("(") != str.npos)
     {
         str.erase(str.begin());
@@ -180,23 +159,18 @@ void operatesum(std::string& str)
     std::string stmp;
     uint64_t num1;
     uint64_t result = 0;
-    char op;
     std::stringstream ss;
     ss << str;
-    bool first = true;
     while (!ss.eof())
     {
         ss >> stmp;
         if (std::stringstream(stmp) >> num1)
-        {
             result += num1;
-        }
     }
-
     str = std::to_string(result);
 }
 
-int CalculateDeepMapParentheses(const std::string& str, std::string& strmap)
+int CalculateDepthMapParentheses(const std::string& str, std::string& strmap)
 {
     strmap.clear();
     uint8_t deep = 0;
@@ -208,7 +182,6 @@ int CalculateDeepMapParentheses(const std::string& str, std::string& strmap)
             deep++;
             maxdeep = maxdeep > deep ? maxdeep : deep;
         }
-
         strmap.append(std::to_string(deep));
 
         if (c == ')')
@@ -217,24 +190,23 @@ int CalculateDeepMapParentheses(const std::string& str, std::string& strmap)
     return maxdeep;
 }
 
-int CalculateDeepMapSum(const std::string& str, std::string& strmap)
+int CalculateDepthMapSum(const std::string& str, std::string& strmap)
 {
     strmap.clear();
     uint8_t deep = 0;
     uint8_t maxdeep = 0;
     for (auto& c : str)
-        strmap.append(std::to_string(deep));
+        strmap.append("0");
 
-    int ini;
-    int end;
-    int lastfound = 0;
+    int ini = 0;
+    int end = 0;
     while (true)
     {
-        int pos = str.find("+" , lastfound + 1);
-        lastfound = pos;
+        int pos = str.find("+" , end);
         if (pos == str.npos)
             return maxdeep;
 
+        maxdeep = 1;
         ini = str.rfind(" ", pos - 2) + 1;
         end = str.find("*", pos);
         if (end == str.npos)
@@ -243,10 +215,6 @@ int CalculateDeepMapSum(const std::string& str, std::string& strmap)
             end -= 2;
 
         for (int i = ini; i <= end; ++i)
-        {
             strmap[i] = '1';
-            maxdeep = 1;
-        }
-
     }
 }
